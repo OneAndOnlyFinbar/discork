a7bc6f0f1e0efe44e99255a4d218c401037a6039
Finbar <finbar@finbar.xyz>
2023-07-11 13:36:47 -0400

    remove duplicate exports

--- src/Classes/index.ts
+++ src/Classes/index.ts
@@ -1,7 +1,4 @@
 export * from './Base';
 export * from './Client';
 export * from './ClientUser';
-export * from '../Managers/GatewayManager';
-export * from '../Managers/IntentsManager';
 export * from './User';
-export * from '../Managers/UserFlagsManager';
